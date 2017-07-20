9951eb90fffff4df735b96efe361a47b72fc89c6
farooqmajeed <farooq306@hotmail.com>
2017-07-20 18:13:50 +0500

    Routing

---
+++ src/Router.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components//Auth/LoginForm';
+import SignupForm from './components/Auth/SignUp';
+import Dashboard from './components/dashboard';
+import ForgotPassword from './components/Auth/ForgotPassword';
+
+const RouterComponent = () => {
+    return (
+        <Router sceneStyle={{ paddingTop: 65 }}>
+
+            <Scene key="auth">
+                <Scene key="login" component={LoginForm} hideNavBar={true} />
+                <Scene key="signup" component={SignupForm} hideNavBar={true} />
+                <Scene key="forgotPassword" component={ForgotPassword} hideNavBar={true} />
+            </Scene>
+
+            <Scene key="main">
+                <Scene key="dashboard" component={Dashboard} hideNavBar={true} initial />
+            </Scene>
+
+        </Router>
+    );
+};
+
+export default RouterComponent;
+
