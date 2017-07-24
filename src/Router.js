abcaa3cbc8075c2601c52e9800e3be7c656e1f40
farooqmajeed <farooq306@hotmail.com>
2017-07-24 17:07:18 +0500

    Routing

--- src/Router.js
+++ src/Router.js
@@ -8,15 +8,15 @@ import ForgotPassword from './components/Auth/ForgotPassword';
 const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 65 }}>
-
             <Scene key="auth">
                 <Scene key="login" component={LoginForm} hideNavBar={true} />
                 <Scene key="signup" component={SignupForm} hideNavBar={true} />
                 <Scene key="forgotPassword" component={ForgotPassword} hideNavBar={true} />
             </Scene>
 
+
             <Scene key="main">
-                <Scene key="dashboard" component={Dashboard} hideNavBar={true} initial />
+                <Scene key="dashboard" component={Dashboard} hideNavBar={true}  />
             </Scene>
 
         </Router>
